fddbe38e0edc3d25e4cfbf2cc8dc4df3e8e40114
Lincoln Jose Alvares Irano <lincoln.jairano@gmail.com>
2021-03-23 06:23:33 -0300

    Starter boilerplate with NextJS, Typescript, Jest and Styled Components

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,10 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Title>NextJS Boilerplate</S.Title>
+    <S.Description>TypeScript, NextJS e Styled Components</S.Description>
+  </S.Wrapper>
+)
+
+export default Main
